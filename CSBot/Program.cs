1f01415e43e94a323ea3e41ba5a744685f2d9cbf
ScottNZ <stealtharcher.scott@gmail.com>
2014-09-18 20:09:44 +1200

    Allow users to specify setup filename

--- CSBot/Program.cs
+++ CSBot/Program.cs
@@ -4,10 +4,12 @@ namespace CSBot
 {
 	public static class Program
 	{
-		static void Main()
+		static void Main(string[] args)
 		{
+			var setupFilename = args.Length > 0 ? args[0] : "CSBot.xml";
+
 			IrcClientSetup setup;
-			using (var streamReader = new StreamReader("CSBot.xml"))
+			using (var streamReader = new StreamReader(setupFilename))
 				setup = IrcClientSetup.Deserialize(streamReader);
 
 			var moduleManager = new ModuleManager();
